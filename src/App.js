f2bb294a44cc211d84dc08e2a5d80986ef796546
RLiuW <rliuworks@gmail.com>
2024-01-18 06:16:50 -0500

    changes made from theia lab environment

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ import Budget from './components/Budget';
 
 
 import { AppProvider } from './context/AppContext';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+
 const App = () => {
     return (
         <AppProvider>
@@ -17,26 +23,47 @@ const App = () => {
                     <div className='row mt-3'>
                         {
                             /* Add Budget component here */
+                                            // Budget component
+                    <div className='col-sm'>
+                    <Budget />
+                </div>
                         }        
 
                         {
                             /* Add Remaining component here*/
+                                             //Remaining component
+                    <div className='col-sm'>
+                    <Remaining />
+                </div>
                         }        
 
                         {
                             /* Add ExpenseTotal component here */
+                                             //ExpenseTotal component
+                    <div className='col-sm'>
+                    <ExpenseTotal />
+                </div>
                         }        
                        
                         {
                             /* Add ExpenseList component here */
+                            <div className='col-sm'>
+                    <ExpenseList />
+                </div>
                         }         
 
                         {
                             /* Add ExpenseItem component here */
+                            <div className='col-sm'>
+                    <ExpenseItem />
+                </div>
                         }        
 
                         {
                             /* Add AllocationForm component here under */
+                            <div className='col-sm'>
+                    <AllocationForm />
+                </div>
                         }        
 
                 </div>
